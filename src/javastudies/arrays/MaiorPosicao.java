f3c03b4cbbe33be1773fc33ad561ddaaf7e43891
Eryka Marcal <erykamarcalt@gmail.com>
2026-06-15 23:58:25 -0300

    Initial commit

---
+++ src/javastudies/arrays/MaiorPosicao.java
@@ -0,0 +1,29 @@
+package javastudies.arrays;
+
+import java.util.Scanner;
+
+public class MaiorPosicao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        double[] vetorDeNumeros = new double[n];
+
+        for (int i = 0; i < vetorDeNumeros.length; i++) {
+        vetorDeNumeros[i] = scanner.nextDouble();
+        }
+        double maiorNumero = vetorDeNumeros[0];
+        int posicaoMaiorNumero = 0;
+
+        for (int i = 1; i < vetorDeNumeros.length; i++) {
+            if (vetorDeNumeros[i] > maiorNumero){
+                maiorNumero = vetorDeNumeros[i];
+                posicaoMaiorNumero = i;
+            }
+        }
+            System.out.println(maiorNumero);
+            System.out.println(posicaoMaiorNumero);
+
+        scanner.close();
+    }
+}
