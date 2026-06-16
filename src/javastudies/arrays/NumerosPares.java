a0959d85cebe2ef3ed58493d3b2232e442665039
Eryka Marcal <erykamarcalt@gmail.com>
2026-06-16 00:37:40 -0300

    Solve array exercise 02 - NumerosPares

---
+++ src/javastudies/arrays/NumerosPares.java
@@ -0,0 +1,32 @@
+package javastudies.arrays;
+
+import java.util.Scanner;
+
+public class NumerosPares {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] vetorDeNumeros = new int[n];
+
+        int qtdPares = 0;
+
+        for (int i = 0; i < vetorDeNumeros.length; i++) {
+            vetorDeNumeros[i] = scanner.nextInt();
+            if (vetorDeNumeros[i] % 2 == 0) {
+                qtdPares++;
+            }
+        }
+
+        for (int numero : vetorDeNumeros) {
+            if (numero % 2 == 0) {
+                System.out.print(numero + " ");
+            }
+        }
+
+        System.out.println("\nQuantidade de Pares: " + qtdPares);
+
+        scanner.close();
+    }
+}
+
