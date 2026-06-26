4702183bed49a5c4791cc2a2a5ac7d1ee9c09099
Eryka Marcal <erykamarcalt@gmail.com>
2026-06-26 12:24:33 -0300

    Solve OOP exercise 01 - Triangle Area

---
+++ src/javastudies/poo/triangle/application/Program.java
@@ -0,0 +1,41 @@
+package javastudies.poo.triangle.application;
+
+import javastudies.poo.triangle.entities.Triangle;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Triangle triangleX = new Triangle();
+        Triangle triangleY = new Triangle();
+
+        System.out.println("Enter the measures of triangle X: ");
+
+        triangleX.ladoA = scanner.nextDouble();
+        triangleX.ladoB = scanner.nextDouble();
+        triangleX.ladoC = scanner.nextDouble();
+
+        System.out.println("Enter the measures of triangle Y: ");
+        triangleY.ladoA = scanner.nextDouble();
+        triangleY.ladoB = scanner.nextDouble();
+        triangleY.ladoC = scanner.nextDouble();
+
+        double areaTriangleX = triangleX.area();
+        double areaTriangleY = triangleY.area();
+
+        System.out.printf("Triangle X area: %.4f%n", areaTriangleX);
+        System.out.printf("Triangle Y area: %.4f%n", areaTriangleY);
+
+        if (areaTriangleX > areaTriangleY) {
+            System.out.println("Largest area: X");
+        } else if (areaTriangleY > areaTriangleX){
+            System.out.println("Largest area: Y");
+        } else {
+        System.out.println("The triangles have the same area.");
+    }
+
+        scanner.close();
+    }
+}
