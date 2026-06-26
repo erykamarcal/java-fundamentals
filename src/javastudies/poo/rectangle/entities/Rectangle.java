e2f20afdef6fb86f5a7e4aff2ac2855ecf8b5f7c
Eryka Marcal <erykamarcalt@gmail.com>
2026-06-26 14:17:38 -0300

    Solve OOP exercise 04 - Rectangle

---
+++ src/javastudies/poo/rectangle/entities/Rectangle.java
@@ -0,0 +1,26 @@
+package javastudies.poo.rectangle.entities;
+
+public class Rectangle {
+    public double width;
+    public double height;
+
+    public double area() {
+        return width * height;
+    }
+
+    public double perimeter() {
+        return 2 * (width + height);
+    }
+
+    public double diagonal() {
+        return Math.sqrt(width * width + height * height);
+    }
+
+    public String toString() {
+
+       return String.format("AREA: %.2f", area() )
+       + String.format("\n PERIMETER: %.2f" , perimeter())
+       + String.format("\n DIAGONAL: %.2f" , diagonal());
+    }
+}
+
