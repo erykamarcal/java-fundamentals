08230e28cead95c20e40536819ff0fff38eebc87
Eryka Marcal <erykamarcalt@gmail.com>
2026-07-14 14:28:32 -0300

    Solve social posts composition exercise

---
+++ src/javastudies/poo/enumAndComposition/socialPosts/entities/Comment.java
@@ -0,0 +1,20 @@
+package javastudies.poo.enumAndComposition.socialPosts.entities;
+
+public class Comment {
+    private String text;
+
+    public Comment(){
+
+    }
+    public Comment (String text){
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
